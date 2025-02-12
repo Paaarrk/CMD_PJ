5890220a3228418347edacb7a93d8e446d4886f0
Your Name <you@example.com>
2025-02-12 15:08:24 +0900

    2.12 lp, kp추가

--- CMD/CommandPrmptOne.cpp
+++ CMD/CommandPrmptOne.cpp
@@ -8,6 +8,7 @@
 #include <tchar.h>
 #include <locale.h>
 #include <Windows.h>
+#include <TlHelp32.h>
 
 
 #define STR_LEN		256
@@ -31,11 +32,18 @@ TCHAR* context = nullptr;
 TCHAR cmdBuffer[STR_LEN];
 TCHAR space[] = _T(" ");
 typedef enum { ARGCON = 1, ARGCOFF = 2};
+typedef enum {_INVALID_HANDLE_VALUE = 100, _PROCESS32_ERROR = 101, _NOT_FOUND = 102, _KILL_FAILED};
+
+int InvokeLP();
+int InvokeKP();
+
+const TCHAR* error[] = { _T("_INVALID_HANDLE_VALUE"), _T("_PROCESS32_ERROR"), _T("_NOT_FOUND"), _T("_KILL_FAILED")};
 int _tmain(int argc, TCHAR* argv[])
 {
 	//한글 입력 가능케 하기 위해
 	_tsetlocale(LC_ALL, _T("Korean"));
 	_tprintf(_T("나의 CMD \n01.21: v1.1, start, echo추가\n\n"));
+	_tprintf(_T("나의 CMD \n02.12: v1.2, lp, kp추가\n\n"));
 	int count = 1;
 	while (argc > count)
 	{
@@ -72,6 +80,7 @@ int _tmain(int argc, TCHAR* argv[])
 
 int CmdProcessing(int a)
 {
+	int result = TRUE;
 	if (a == ARGCON)
 		goto ARGC_ON;
 	_fputts(_T("Best command prompt>> "), stdout);
@@ -91,7 +100,8 @@ ARGC_ON:
 		else {
 			if (!_tcscmp(_T("echo"), StrLower(token)))
 				isEcho = TRUE;
-
+			else if (!_tcscmp(_T("kp"), StrLower(token)))
+				isEcho = TRUE;
 			_tcscpy_s(cmdTokenList[tokenNum++], StrLower(token));
 			token = _tcstok_s(NULL, seps, &context);
 		}
@@ -133,6 +143,22 @@ ARGC_ON:
 		_tprintf_s(_T("%s\n\n"), cmdBuffer);
 
 	}
+	else if (!_tcscmp(cmdTokenList[0], _T("lp")))
+	{
+		result = InvokeLP();
+		if (result != TRUE)
+		{
+			_tprintf_s(_T("%d : %s\n"), result, error[result-100]);
+		}
+	}
+	else if (!_tcscmp(cmdTokenList[0], _T("kp")))
+	{
+		result = InvokeKP();
+		if (result != TRUE)
+		{
+			_tprintf_s(_T("%d : %s\n"), result, error[result - 100]);
+		}
+	}
 	else
 	{
 		STARTUPINFO si = { 0, };
@@ -140,7 +166,7 @@ ARGC_ON:
 		si.cb = sizeof(si);
 		//SetCurrentDirectory(_T("C:\\WINDOWS\\system32"));
 		bool isRun = CreateProcess(
-		NULL, cmdTokenList[0], NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi
+			NULL, cmdTokenList[0], NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi
 		);
 
 		if (isRun == FALSE)
@@ -208,25 +234,78 @@ BOOL MakeStartBuffer(void)
 		_tcscat_s(cmdBuffer, space);
 	}
 	success = TRUE;
-	
+
 	return success;
 }
 
-/*
-함수: void Notepad(void)
-기능: 현재 디렉터리를 system32로 바꾸고  notepad.exe를 실행한다.
-*/
-//void Notepad()
-//{
-//	STARTUPINFO si = { 0, };
-//	PROCESS_INFORMATION pi;
-//	si.cb = sizeof(si);
-//
-//	TCHAR command[] = _T("notepad.exe");
-//	SetCurrentDirectory(_T("C:\\WINDOWS\\system32"));
-//
-//	ZeroMemory(&pi, sizeof(pi));
-//	CreateProcess(
-//		NULL, command, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi
-//	);
-//}
+static int InvokeLP()
+{
+	int count = 0;
+	HANDLE hProcessSnap =
+		CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hProcessSnap == INVALID_HANDLE_VALUE)
+	{
+		_tprintf_s(_T("CreateToolhelp32Snapshot error! "));
+		return _INVALID_HANDLE_VALUE;
+	}
+
+	PROCESSENTRY32 pe32;
+	pe32.dwSize = sizeof(pe32);
+
+	if (!Process32First(hProcessSnap, &pe32))
+	{
+		_tprintf_s(_T("Process32First error! \n"));
+		CloseHandle(hProcessSnap);
+		return _PROCESS32_ERROR;
+	}
+
+	do {
+		_tprintf_s(_T("%40s\t%5d\n"), pe32.szExeFile, pe32.th32ProcessID);
+		count++;
+	} while (Process32Next(hProcessSnap, &pe32));
+	_tprintf_s(_T("Total Process Count: %d\n"), count);
+	CloseHandle(hProcessSnap);
+	return TRUE;
+}
+
+static int InvokeKP()
+{
+	int ProcessId = 0;
+	HANDLE hProcessSnap =
+		CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hProcessSnap == INVALID_HANDLE_VALUE)
+	{
+		_tprintf_s(_T("CreateToolhelp32Snapshot error! "));
+		return _INVALID_HANDLE_VALUE;
+	}
+
+	PROCESSENTRY32 pe32;
+	pe32.dwSize = sizeof(pe32);
+
+	if (!Process32First(hProcessSnap, &pe32))
+	{
+		_tprintf_s(_T("Process32First error! \n"));
+		CloseHandle(hProcessSnap);
+		return _PROCESS32_ERROR;
+	}
+	
+	do {
+		if (_tcscmp(pe32.szExeFile, cmdTokenList[1]) == 0)
+		{
+			ProcessId = pe32.th32ProcessID;
+			break;
+		}
+	} while (Process32Next(hProcessSnap, &pe32));
+	if (ProcessId == 0)
+	{
+		CloseHandle(hProcessSnap);
+		return _NOT_FOUND;
+	}
+	HANDLE hOpen = OpenProcess(PROCESS_ALL_ACCESS, FALSE, ProcessId);
+	BOOL isKill = TerminateProcess(hOpen, -1);
+	CloseHandle(hProcessSnap);
+	CloseHandle(hOpen);
+	if (isKill == FALSE)
+		return _KILL_FAILED;
+	return TRUE;
+}
